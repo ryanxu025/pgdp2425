87dbc0a2db5b064bfe33b2aa8adaec5be29c89ef
Junguo Xu <ryanxu025@gmail.com>
2024-11-10 00:30:22 +0100

    weishenmenamenan

---
+++ pgdp2425w03h03/src/pgdp/Lockpicker.java
@@ -0,0 +1,97 @@
+package pgdp;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Lockpicker {
+
+    private Mysterybox mbox;
+
+    public Lockpicker(Mysterybox box) {
+        this.mbox = box;
+    }
+
+    // ---------------------- Teil 1 Methoden ----------------------
+
+    public String decryptInteger(int ix) {
+        int result = Math.abs(ix * ix * mbox.getIntHint()) % 26;
+        return HelperBook.intToAlphabetCharacter(result);
+    }
+
+    public String decryptFloat(float fx) {
+        // TODO: Implementiere diese Methode
+        double result = Math.abs(Math.cos((fx + mbox.getIntHint()) * mbox.getFloatHint()));
+        long roundedResult = Math.round(result * 1000) % 1000;
+        if (roundedResult == 0) {
+            roundedResult = Math.round(result * 1000) % 10000;
+        }
+        return String.valueOf(roundedResult);
+    }
+
+    public String decryptNumericalString(String sx) {
+        String result = "";
+        for (int i = 0; i < 8; i = i + 2) {
+            result += HelperBook.intToAlphabetCharacter(Integer.parseInt(sx.substring(i, i + 2)) % 26);
+        }
+        return result;
+
+    }
+
+    // ---------------------- Teil 2 Methoden ----------------------
+
+    public Float decryptModular(int a, int b) {
+        // TODO: Implementiere diese Methode
+        float firstResult = a * mbox.getFloatHint();
+        float secondResult = b / mbox.getFloatHint();
+        return firstResult % secondResult;
+    }
+
+    public String decryptMinimal(float x, float y) {
+        // TODO: Implementiere diese Methode
+        float max = Math.max(x, y);
+        float min = Math.min(x, y);
+        double firstResult = Math.sqrt(max);
+        double secondResult = min * mbox.getFloatHint();
+        String roundedFirstResult = String.valueOf(Math.round(firstResult * 10000.0));
+        String roundedSecondResult = String.valueOf(Math.round(secondResult * 10000.0));
+        return (roundedFirstResult).substring(roundedFirstResult.length()-4) +
+               (roundedSecondResult).substring(roundedSecondResult.length()-4);
+
+    }
+
+    public int decryptBytepolar(int i, byte b) {
+        int dividend = i * b;
+        int divisor = ((i % b) << 1) + 1;
+        return dividend / divisor;
+    }
+
+    // ---------------------- Teil 3 Methoden ----------------------
+
+    public boolean solve() {
+
+        String s1 = decryptInteger(decryptBytepolar(mbox.getPolarVal1(), getMbox().getPolarVal2()));
+        String s2 = decryptFloat(decryptModular(mbox.getModularVal1(), mbox.getModularVal2()));
+        String s3 = decryptNumericalString(decryptMinimal(mbox.getMinimalVal1(), mbox.getMinimalVal2()));
+        System.out.println(s1 + "_" + s2 + "_" + s3);
+        return mbox.checkSolution(s1 + "_" + s2 + "_" + s3);
+    }
+
+    // ====================== Helper/Getter/Setter ======================
+
+
+    public Mysterybox getMbox() {
+        return mbox;
+    }
+
+    public void setMbox(Mysterybox mbox) {
+        this.mbox = mbox;
+    }
+
+    public static void main(String[] args) {
+        Lockpicker lockpicker = new Lockpicker(null);
+        for (int i = 0; i < 10000; i++) {
+            lockpicker.setMbox(new Mysterybox(i, ""));
+            lockpicker.solve();
+        }
+    }
+}
